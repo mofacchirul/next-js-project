d2123f416868359fb5546cfd45927c4112f6a205
mofacchirul <mofaccharulhoque@gmail.com>
2025-04-30 11:22:23 +0600

    news page add

---
+++ src/componets/navbar.tsx
@@ -0,0 +1,89 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+ const links = <>
+<Link href={"/news"}>News</Link>
+<li></li>
+ </>
+
+
+    return (
+        <div>
+            <div className="navbar  shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost  lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-black text-white  rounded-box z-1 mt-3 w-52 p-2 shadow">
+       {
+        links
+       }
+      </ul>
+    <h1></h1>
+    </div>
+    
+<h1>
+<Link href={"/"} className="font-bold hidden lg:flex   text-xl" >
+    Daily News
+    </Link>
+</h1>
+  </div>
+ 
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+     {
+        links
+     }
+    </ul>
+   
+  </div>
+  <div className="  lg:hidden  w-full">
+  <Link href={"/"} className="font-bold text-xl">
+    Daily News
+  </Link>
+</div>
+
+  <div className="navbar-end space-x-2">
+    <div>
+    <label className="flex cursor-pointer gap-2">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <circle cx="12" cy="12" r="5" />
+    <path
+      d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+  </svg>
+  <input type="checkbox" value="synthwave" className="toggle theme-controller" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+  </svg>
+</label>
+    </div>
+    <a className="btn">Button</a>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Navbar;
